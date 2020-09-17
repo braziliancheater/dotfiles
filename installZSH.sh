286f553fc38de224316b09505490baf0ad276c21
braziliancheater <gabrielelerlima80@gmail.com>
2020-09-16 22:38:47 -0300

    new install script

---
+++ installZSH.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "<----------------------------------->"
+echo "        zsh installer e plugins      "
+echo "                crim3s..             "
+echo "<----------------------------------->"
+echo "Vamos comecar! - "
+whoami
+
+sudo pacman -S zsh
+
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
